e3cdf28b57518e7af80fcc5dc1106ce4c5d16368
alan <james.grusy@ncr.com>
2019-12-18 15:31:00 -0500

    test change

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "google" {
 }
 
 resource "google_compute_instance" "default" {
-  name         = "test"
+  name         = "test01"
   machine_type = "n1-standard-1"
   zone         = "us-central1-a"
 
